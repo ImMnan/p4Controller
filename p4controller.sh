24cf9865a8b4a77722833cdd7b67dac419cfa586
Manan patel <86557286+ImMnan@users.noreply.github.com>
2025-09-15 16:43:52 +0530

    using hostname if ip is not desired for the pod.

--- p4controller.sh
+++ p4controller.sh
@@ -5,7 +5,15 @@ mkdir -p $ROOT_DIR
 mkdir -p $CHECKPOINT_DIR
 mkdir -p $VERSION_DIR
 
+if [ -z "$P4D_IP" ]; then
+  echo "P4D_IP is not set. Using hostname"
+  P4D_IP=$(hostname)
+  
+fi
+
 p4d -V
+
+echo "Starting p4d with IP: $P4D_IP and PORT: $P4D_PORT \n"
 
 p4d -r $ROOT_DIR -L log -p $P4D_IP:$P4D_PORT -d
 
