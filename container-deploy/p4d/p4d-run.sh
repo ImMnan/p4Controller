82a1ea7137e31f0443815081a7b286f45f6a8eef
Manan patel <86557286+ImMnan@users.noreply.github.com>
2025-09-28 21:22:55 +0530

    changes as per p4c go code

--- container-deploy/p4d/p4d-run.sh
+++ container-deploy/p4d/p4d-run.sh
@@ -5,26 +5,26 @@ if [ -z "$CTR_IP" ]; then
   CTR_IP=$(hostname)
 fi
 
-P4PORT=${CTR_IP}:${CTR_PORT}
+P4PORT=${CTR_IP}:${P4C_PORT}
 
-while [ "$SERVER_INIT" != "true" ]; do
+while [ "$P4C_INIT" != "true" ]; do
   echo "Waiting for p4Controller to initialize the server..."
   sleep 30
 done
 
-if [ "$SERVER_TYPE" = "master" ]; then
+if [ "$P4C_SERVER_TYPE" = "master" ]; then
   echo "Starting p4d commit-server with: $P4PORT"
 else
   echo "Starting replica server with: $P4PORT"
 fi
 
-if [ -z "$RUN_COMMAND" ]; then
+if [ -z "$P4C_RUN_COMMAND" ]; then
   echo "RUN_COMMAND is not set. Using default command"
-  RUN_COMMAND="p4d -r \"$P4ROOT\" -p \"$P4PORT\" -d"
+  P4C_RUN_COMMAND="p4d -r \"$P4ROOT\" -p \"$P4PORT\" -d"
 fi
 
-echo "Running command: $RUN_COMMAND"
-eval "$RUN_COMMAND"
+echo "Running command: $P4C_RUN_COMMAND"
+eval "$P4C_RUN_COMMAND"
 
 # Keep the container running
 while true; do
